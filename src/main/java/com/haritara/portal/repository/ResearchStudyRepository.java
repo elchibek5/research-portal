c94fca600f35555833a9992ea8b4be25c84607f6
elchibekdastanov <elchibekdastanov@gmail.com>
2026-03-04 01:19:19 -0600

    interface

---
+++ src/main/java/com/haritara/portal/repository/ResearchStudyRepository.java
@@ -0,0 +1,8 @@
+package com.haritara.portal.repository;
+
+import com.haritara.portal.model.ResearchStudy;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ResearchStudyRepository extends JpaRepository<ResearchStudy, Long> {
+
+}
