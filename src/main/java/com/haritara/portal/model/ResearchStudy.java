38bc83d4707fbbc19c3cfae6c2be258a5693cd4a
elchibekdastanov <elchibekdastanov@gmail.com>
2026-03-04 01:18:58 -0600

    entity created

---
+++ src/main/java/com/haritara/portal/model/ResearchStudy.java
@@ -0,0 +1,28 @@
+package com.haritara.portal.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "studies")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class ResearchStudy {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String title;
+
+    @Column(columnDefinition = "TEXT")
+    private String description;
+
+    private String eligibility;
+    private String compensation;
+    private String timeline;
+}
