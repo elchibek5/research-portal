b366db1892077933c66c160decccbafe700dca3b
elchibekdastanov <elchibekdastanov@gmail.com>
2026-05-03 04:27:18 -0500

    sync backend

--- src/main/java/com/haritara/portal/ResearchPortalApplication.java
+++ src/main/java/com/haritara/portal/ResearchPortalApplication.java
@@ -2,8 +2,10 @@ package com.haritara.portal;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing  // Enable JPA auditing for @CreatedDate and @LastModifiedDate (Critical #1)
 public class ResearchPortalApplication {
 
 	public static void main(String[] args) {
@@ -11,3 +13,5 @@ public class ResearchPortalApplication {
 	}
 
 }
+
+
